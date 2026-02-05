e475fa4586eec35a64f06af742aa0ff2b8841044
Manos <manosnikitakis3@gmail.com>
2026-02-05 13:24:40 +0200

    Add song controller for getting all songs, by id, featured, trending and made for you

---
+++ Backend/src/controllers/song.controller.js
@@ -0,0 +1,85 @@
+import { image } from "framer-motion/client";
+import { Song } from "../models/song.model.js";
+
+export const getAllSongs = async (req, res, next) => {
+    try {
+        const songs = await Song.find().sort({ createdAt: -1 });
+        res.status(200).json(song);
+    }catch (error) {
+        next(error);
+    }
+    
+}
+
+export const getSongById = async (req, res, next) => {
+
+}
+
+export const getFeaturedSongs = async (req, res, next) => {
+    try{
+        const featuredSongs = await Song.aggregate([
+            {
+                $sample: { size: 6 }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                }
+            }
+        ])
+
+        res.status(200).json(featuredSongs);
+    }catch (error) {
+        next(error);
+    }   
+}
+
+export const getMadeForYouSongs = async (req, res, next) => {
+   try{
+        const madeForYouSongs = await Song.aggregate([
+            {
+                $sample: { size: 4 }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                }
+            }
+        ])
+
+        res.status(200).json(featuredSongs);
+    }catch (error) {
+        next(error);
+    }   
+}
+
+export const getTrendingSongs = async (req, res, next) => {
+    try{
+        const trendingSongs = await Song.aggregate([
+            {
+                $sample: { size: 4 }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    title: 1,
+                    artist: 1,
+                    imageUrl: 1,
+                    audioUrl: 1,
+                }
+            }
+        ])
+
+        res.status(200).json(featuredSongs);
+    }catch (error) {
+        next(error);
+    } 
+}
