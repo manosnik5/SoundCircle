36eab3936d8496004aa1f4d1dd16a5b2bf7cc973
Manos <manosnikitakis3@gmail.com>
2026-02-05 13:28:52 +0200

    Add routes for checkAdmin, createAlbum, deleteAlbum

--- Backend/src/routes/admin.route.js
+++ Backend/src/routes/admin.route.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { createSong, deleteSong } from "../controllers/admin.controller.js";
+import { createSong, deleteSong, createAlbum, deleteAlbum, checkAdmin } from "../controllers/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.post("/songs", protectRoute, requireAdmin, createSong);
-router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
+router.use(protectRoute, requireAdmin);
+
+router.get("/check", checkAdmin);
+
+router.post("/songs", createSong);
+router.delete("/songs/:id", deleteSong);
+
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router;
