176579ff0c4c8b3b371ef0401cf63bb65e883406
Manos <manosnikitakis3@gmail.com>
2026-02-05 13:30:08 +0200

    Add routes for getAllSongs, getFeaturedSongs, getMadeForYouSongs, getTrendingSongs, getSongById

--- Backend/src/routes/song.route.js
+++ Backend/src/routes/song.route.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { getAllSongs, getSongById,getFeaturedSongs,getMadeForYouSongs, getTrendingSongs} from "../controllers/song.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("User route is working");
-});
+router.get("/",protectRoute, requireAdmin, getAllSongs);
+router.get("/featured", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
+router.get("/:songId", getSongById);
 
 export default router;
