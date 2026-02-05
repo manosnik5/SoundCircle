2c27a6090720cb18268018f256bc15c87d239092
Manos <manosnikitakis3@gmail.com>
2026-02-05 11:55:13 +0200

    Add Admin route

---
+++ Backend/src/routes/admin.route.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createSong, deleteSong } from "../controllers/admin.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+
+const router = Router();
+
+router.post("/songs", protectRoute, requireAdmin, createSong);
+router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
+
+export default router;
