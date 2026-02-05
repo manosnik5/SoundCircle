8d6d7c59940aefbf40fb3848ce43be84e1465d6e
Manos <manosnikitakis3@gmail.com>
2026-02-05 13:30:26 +0200

    Add routes for getAllUsers

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllUsers } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.get("/like", protectRoute, (req, res) => {
-    req.auth.userId;
-    res.send("User route is working");
-});
+router.get("/", protectRoute, getAllUsers)
 
 export default router;
