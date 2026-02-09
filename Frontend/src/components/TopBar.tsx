6f02a549dca48d340f7aca4c7b2107364d758c2e
Manos <manosnikitakis3@gmail.com>
2026-02-09 11:20:33 +0200

    Add top bar component

---
+++ Frontend/src/components/TopBar.tsx
@@ -0,0 +1,33 @@
+import { SignedOut } from '@clerk/clerk-react';
+import { LayoutDashboardIcon } from 'lucide-react';
+import SignInOAuthButton from './SignInOAuthButton';
+import { Link } from 'react-router-dom';
+
+const TopBar = () => {
+    const isAdmin = false;
+  return (
+    <div className='flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10'>
+        <div className='flex gap-2 items-center'>
+            logo
+        </div>
+        <div className='flex gap-4 items-center'>
+            {
+                isAdmin ? (
+                    <Link to={"/admin"}>
+                        <LayoutDashboardIcon className='size-4 mr-2'/>
+                        Admin DashBoard
+                    </Link>
+                ) : (
+                    <SignedOut>
+                        <SignInOAuthButton/>
+                    </SignedOut>
+                )
+            }
+
+           
+        </div>
+    </div>
+  )
+}
+
+export default TopBar
