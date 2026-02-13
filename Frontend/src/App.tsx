7c7b3b110d7f64f68b12a9cbd50256ec5ecf75fd
Manos <manosnikitakis3@gmail.com>
2026-02-13 12:13:34 +0200

    add sso-callback auth-callback and mainlayout routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,28 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { AuthenticateWithRedirectCallback} from '@clerk/clerk-react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage';
+import MainLayout from './layout/MainLayout';
+import ChatPage from './pages/chat/ChatPage';
+import AlbumPage from './pages/album/AlbumPage';
+
 
 function App() {
+
   return (
-    <header>
-      <SignedOut>
-        <SignInButton>
-          <button className='bg-black text-white px-4 py-2 cursor-pointer'>Sign In</button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+   <>
+    <Routes>
+      <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signInForceRedirectUrl={"auth-callback"}/>} />
+
+      <Route path="/auth-callback" element={<AuthCallbackPage/>} />
+      
+      <Route element={<MainLayout/>}>
+         <Route path="/" element={<HomePage/>} />
+         <Route path="/chat" element={<ChatPage/>} />
+         <Route path="/albums/:albumId" element={<AlbumPage/>} />
+      </Route>
+    </Routes>
+   </>
   );
 }
 
