7fc935692f4a577612f494e7633ea662b2b8e3f8
Manos <manosnikitakis3@gmail.com>
2026-02-18 12:44:36 +0200

    add api method for seaching songs and albums

--- Frontend/src/api/music.ts
+++ Frontend/src/api/music.ts
@@ -3,10 +3,21 @@ import type { Album, Song, User } from "@/types";
 
 export const musicApi = {
     getSongs: async (): Promise<Song[]> => {
-    const response = await axiosInstance.get("/songs");
-    return response.data;
-  },
-
+        const response = await axiosInstance.get("/songs");
+        return response.data;
+    },
+    getFeaturedSongs: async (): Promise<Song[]> => {
+        const response = await axiosInstance.get("/songs/featured");
+        return response.data;
+    },
+    getMadeForYouSongs: async (): Promise<Song[]> => {
+        const response = await axiosInstance.get("/songs/made-for-you");
+        return response.data;
+    },
+    getTrendingSongs: async (): Promise<Song[]> => {
+        const response = await axiosInstance.get("/songs/trending");
+        return response.data;
+    },
     getAlbums: async (): Promise<Album[]> => {
         const response = await axiosInstance.get("/albums");
         return response.data;
@@ -18,6 +29,18 @@ export const musicApi = {
     getUsers: async (): Promise<User[]> => {
         const response = await axiosInstance.get("/users");
         return response.data;
-    }
+    },
+    checkAdmin: async (): Promise<{ admin: boolean }> => {
+        const response = await axiosInstance.get("/admin/check");
+        return response.data;
+    },
+    searchSongs: async (query: string): Promise<Song[]> => {
+        const response = await axiosInstance.get(`/songs/search?q=${encodeURIComponent(query)}`);
+        return response.data;
+    },
+    searchAlbums: async (query: string): Promise<Album[]> => {
+        const response = await axiosInstance.get(`/albums/search?q=${encodeURIComponent(query)}`);
+        return response.data;
+    },
 }
 
