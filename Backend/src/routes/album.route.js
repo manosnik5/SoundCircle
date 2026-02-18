2913023f342ee5050577945aab403b1ccbc8c92b
Manos <manosnikitakis3@gmail.com>
2026-02-18 12:43:22 +0200

    add route for search album

--- Backend/src/routes/album.route.js
+++ Backend/src/routes/album.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAllAlbums, getAlbumById} from "../controllers/album.controller.js";
+import { getAllAlbums, getAlbumById, getSearchAlbums} from "../controllers/album.controller.js";
 
 const router = Router();
 
 router.get("/", getAllAlbums);
+router.get("/search", getSearchAlbums);
 router.get("/:albumId", getAlbumById);
 
 export default router;
