57383a5866ebf30b5138a72c55f111088588d3d4
Manos <manosnikitakis3@gmail.com>
2026-02-18 12:43:45 +0200

    add route for search song

--- Backend/src/routes/song.route.js
+++ Backend/src/routes/song.route.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { getAllSongs, getSongById,getFeaturedSongs,getMadeForYouSongs, getTrendingSongs} from "../controllers/song.controller.js";
+import { getAllSongs,getFeaturedSongs,getMadeForYouSongs, getTrendingSongs, getSearchSongs} from "../controllers/song.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.get("/",protectRoute, requireAdmin, getAllSongs);
 router.get("/featured", getFeaturedSongs);
+router.get("/search", getSearchSongs);
 router.get("/made-for-you", getMadeForYouSongs);
 router.get("/trending", getTrendingSongs);
-router.get("/:songId", getSongById);
 
 export default router;
