c4cbdc539ccfadb6b17c8d237aced65fdb8e7430
Manos <manosnikitakis3@gmail.com>
2026-02-18 12:41:52 +0200

    implement PlayButton for controlling MusicPlayer playback

---
+++ Frontend/src/pages/home/components/PlayButton.tsx
@@ -0,0 +1,26 @@
+import type { Song } from '@/types'
+import { usePlayer } from '@/contexts/MusicPlayerContext'
+import { Button } from '@/components/ui/button';
+import { icons, Pause, Play } from 'lucide-react';
+
+
+const PlayButton = ({song}: {song: Song}) => {
+    const { currentSong, isPlaying, setCurrentSong, togglePlay} = usePlayer();
+    const isCurrentSong = currentSong?._id === song._id;
+
+    const handlePlay = () => {
+        if (isCurrentSong) togglePlay();
+        else setCurrentSong(song)
+    }
+  return (
+    <Button size={"icon"} onClick={handlePlay} className={`absolute bottom-3 right-2 bg-green-500 hover:bg-green-400 hover:scale-105 transition-all opacity-0 translate-y-2 group-hover:translate-y-0 cursor-pointer ${isCurrentSong ? "opacity-100" : "opacity-0 group-hover:opacity-100"}`}>
+        {isCurrentSong && isPlaying ? (
+            <Pause className='size-5 text-black'/>
+        ) : (
+            <Play className='size-5 text-black'/>
+        )}
+    </Button>
+  )
+}
+
+export default PlayButton
