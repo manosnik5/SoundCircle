17f9e1d89b080e77d41cd32d1d641d9f6ddd031a
Manos <manosnikitakis3@gmail.com>
2026-03-01 11:46:44 +0200

    add toaster for better messages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,6 +5,8 @@ import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage';
 import MainLayout from './layout/MainLayout';
 import ChatPage from './pages/chat/ChatPage';
 import AlbumPage from './pages/album/AlbumPage';
+import AdminPage from './pages/admin/AdminPage';
+import { Toaster } from "react-hot-toast"
 
 
 function App() {
@@ -14,7 +16,8 @@ function App() {
     <Routes>
       <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signInForceRedirectUrl={"auth-callback"}/>} />
 
-      <Route path="/auth-callback" element={<AuthCallbackPage/>} />
+      <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
+      <Route path="/admin" element={<AdminPage/>}/>
       
       <Route element={<MainLayout/>}>
          <Route path="/" element={<HomePage/>} />
@@ -22,6 +25,7 @@ function App() {
          <Route path="/albums/:albumId" element={<AlbumPage/>} />
       </Route>
     </Routes>
+    <Toaster/>
    </>
   );
 }
