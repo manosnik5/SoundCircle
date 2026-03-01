b42c6eb0246fdd0e44aa9cb21d62822b918748b9
Manos <manosnikitakis3@gmail.com>
2026-03-01 11:53:51 +0200

    fixed stat controller

--- Backend/src/controllers/stat.controller.js
+++ Backend/src/controllers/stat.controller.js
@@ -2,38 +2,25 @@ import {Song} from "../models/song.model.js";
 import {Album} from "../models/album.model.js";
 import { User } from "../models/user.model.js";
 
-export const getAllStats = async (req, res, next) => {
+export const getStats = async (req, res, next) => {
     try {
         const [totalSongs, totalAlbums, totalUsers, uniqueArtists] = await Promise.all([
             Song.countDocuments(),
             Album.countDocuments(),
             User.countDocuments(),
-
-            Song.aggregate([
-                {
-                    $unionWith: {
-                        coll: "albums",
-                        pipeline: [] 
-                    },
-                },
-                {
-                    $group: {
-                        _id: "$artist",
-                    },
-                },
-                {
-                    $count: "count",
-                }
-            ])
+            
+            // Get unique artists from songs only
+            Song.distinct("artist")
         ]);
-    res.status(200).json({
-        totalSongs,
-        totalAlbums,
-        totalUsers,
-        totalArtists: uniqueArtists[0]?.count || 0,
-    });
+
+        res.status(200).json({
+            totalSongs,
+            totalAlbums,
+            totalUsers,
+            totalArtists: uniqueArtists.length, // distinct returns array
+        });
         
-    }catch (error) {
+    } catch (error) {
         next(error);
     }
 }
