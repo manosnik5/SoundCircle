627d7d029ceccdb29c7d7419f82264b850b7d1bd
Manos <manosnikitakis3@gmail.com>
2026-03-01 11:52:17 +0200

    fixed a typo

--- Backend/src/routes/song.route.js
+++ Backend/src/routes/song.route.js
@@ -4,7 +4,7 @@ import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/",protectRoute, requireAdmin, getAllSongs);
+router.get("/", protectRoute, requireAdmin, getAllSongs);
 router.get("/featured", getFeaturedSongs);
 router.get("/search", getSearchSongs);
 router.get("/made-for-you", getMadeForYouSongs);
