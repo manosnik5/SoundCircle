c2d4333be6d27677237f217e45b7d2d502fd5b66
Manos <manosnikitakis3@gmail.com>
2026-03-01 11:39:23 +0200

    add dashboard with stats component

---
+++ Frontend/src/pages/admin/components/DashBoardStats.tsx
@@ -0,0 +1,62 @@
+import { useStats } from "@/hooks/useMusic";
+import StatsCard from "./StatsCard";
+import { Library, ListMusic, PlayCircle, Users2 } from "lucide-react";
+
+const DashBoardStats = () => {
+ 
+    const { data: stats, isLoading, error } = useStats();
+
+    if (isLoading) {
+    	return <div>Loading stats...</div>;
+	}
+
+	if (error) {
+		return <div>Error loading stats</div>;
+	}
+		
+     const statsData = [
+		{
+			icon: ListMusic,
+			label: "Total Songs",
+			value: stats?.totalSongs.toString(),
+			bgColor: "bg-emerald-500/10",
+			iconColor: "text-emerald-500",
+		},
+		{
+			icon: Library,
+			label: "Total Albums",
+			value: stats?.totalAlbums.toString(),
+			bgColor: "bg-violet-500/10",
+			iconColor: "text-violet-500",
+		},
+		{
+			icon: Users2,
+			label: "Total Artists",
+			value: stats?.totalArtists.toString(),
+			bgColor: "bg-orange-500/10",
+			iconColor: "text-orange-500",
+		},
+		{
+			icon: PlayCircle,
+			label: "Total Users",
+			value: stats?.totalUsers.toLocaleString(),
+			bgColor: "bg-sky-500/10",
+			iconColor: "text-sky-500",
+		},
+	];
+  return (
+    <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
+     {statsData.map((stat) => (
+      <StatsCard 
+	  	key={stat.label}
+		icon={stat.icon}
+		label={stat.label}
+		value={stat.value}
+		bgColor={stat.bgColor}
+		iconColor={stat.iconColor}/>
+     ))}
+    </div>
+  )
+}
+
+export default DashBoardStats
