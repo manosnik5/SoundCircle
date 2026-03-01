12b74c175e1139ac6212ef722a410bd7cd3d7560
Manos <manosnikitakis3@gmail.com>
2026-03-01 11:56:08 +0200

    updated names

--- Backend/src/routes/stat.route.js
+++ Backend/src/routes/stat.route.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
-import { getAllStats } from "../controllers/stat.controller.js";
-import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import express from 'express';
+import { getStats } from '../controllers/stat.controller.js';
+import { protectRoute, requireAdmin } from '../middleware/auth.middleware.js';
 
-const router = Router();
+const router = express.Router();
 
-router.get("/", protectRoute, requireAdmin, getAllStats);
+router.get('/', protectRoute, requireAdmin, getStats);
 
 export default router;
