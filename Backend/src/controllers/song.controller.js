c7b33cc39ab27d887bc7ec2ff5db2fdc4da9c8a1
Manos <manosnikitakis3@gmail.com>
2026-03-01 11:55:02 +0200

    fixed a typo (song) -> (songs)

--- Backend/src/controllers/song.controller.js
+++ Backend/src/controllers/song.controller.js
@@ -4,7 +4,7 @@ import { Song } from "../models/song.model.js";
 export const getAllSongs = async (req, res, next) => {
     try {
         const songs = await Song.find().sort({ createdAt: -1 });
-        res.status(200).json(song);
+        res.status(200).json(songs);
     }catch (error) {
         next(error);
     }
