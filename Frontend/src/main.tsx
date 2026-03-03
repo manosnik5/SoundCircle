b52c9a3b25c6f77fe1f4316f24a525ff2dcaaf3f
Manos <manosnikitakis3@gmail.com>
2026-03-03 12:54:46 +0200

    wrapped with SocketProvider

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -8,6 +8,7 @@ import AuthProvider from './providers/AuthProvider.tsx'
 import {QueryClientProvider} from '@tanstack/react-query'
 import {queryClient} from '@/lib/queryClient.ts'
 import { MusicPlayerProvider } from './contexts/MusicPlayerContext.tsx'
+import { SocketProvider } from './contexts/SocketContext.tsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <QueryClientProvider client={queryClient}>
+        <SocketProvider>
         <MusicPlayerProvider>
           <AuthProvider>
             <BrowserRouter>
@@ -26,6 +28,7 @@ createRoot(document.getElementById('root')!).render(
             </BrowserRouter>
         </AuthProvider>
       </MusicPlayerProvider>
+      </SocketProvider>
       </QueryClientProvider>  
     </ClerkProvider>
   </StrictMode>,
