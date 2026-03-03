8811ee0539793f0c8d5f53b1b178f924da0b49de
Manos <manosnikitakis3@gmail.com>
2026-03-03 12:46:04 +0200

    add getMessage function

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.model.js";
+import { Message } from "../models/message.model.js"
 
 export const getAllUsers = async (req, res, next) => {
     try {
@@ -11,3 +12,23 @@ export const getAllUsers = async (req, res, next) => {
         next(error);
     }
 }
+
+export const getMessages = async (req, res, next) => {
+    try {
+        const myId = req.auth.userId;
+        const {userId} = req.params;
+        
+        
+        const messages = await Message.find({
+            $or: [
+                {senderId: userId, receiverId: myId},
+                {senderId: myId, receiverId: userId}
+            ]
+        }).sort({createdAt: 1})
+        res.status(200).json(messages)
+    }catch (error) {
+        next(error);
+    }
+}
+
+
