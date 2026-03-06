b7295f0ac5a301f7d0b40cf2bd2c938749ec1717
Manos <manosnikitakis3@gmail.com>
2026-03-06 11:46:59 +0200

    improve audio player useEffect logic

--- Frontend/src/layout/components/AudioPlayer.tsx
+++ Frontend/src/layout/components/AudioPlayer.tsx
@@ -5,47 +5,42 @@ const AudioPlayer = () => {
   const audioRef = useRef<HTMLAudioElement>(null)
   const prevSongRef = useRef<string | null>(null)
 
-  const {currentSong, isPlaying, playNext} = usePlayer();
+  const { currentSong, isPlaying, playNext } = usePlayer();
 
-  useEffect(() => {
-    if (isPlaying) audioRef.current?.play();
-    else audioRef.current?.pause();
-  }, [isPlaying]);
-
-  useEffect(() => {
-    const handleEnded = () => {
-      playNext()
-    }
-
-    audioRef.current?.addEventListener("ended", handleEnded)
-
-    return () => audioRef.current?.removeEventListener("ended", handleEnded)
-  }, [playNext]);
-
-  useEffect(() => {
-  if (!audioRef.current || !currentSong) return;
-
-  if (prevSongRef.current !== currentSong.audioUrl) {
-    audioRef.current.src = currentSong.audioUrl;
-    audioRef.current.currentTime = 0;
-    prevSongRef.current = currentSong.audioUrl;
-    audioRef.current.play();
-  }
-}, [currentSong]);
 
   useEffect(() => {
     if (!audioRef.current) return;
-
-    if (isPlaying) {
-      audioRef.current.play();
-    } else {
-      audioRef.current.pause();
-    }
+    if (isPlaying) audioRef.current.play();
+    else audioRef.current.pause();
   }, [isPlaying]);
 
 
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (!audio) return;
 
-  return <audio ref={audioRef}/>
+    const handleEnded = () => playNext();
+    audio.addEventListener("ended", handleEnded);
+    return () => audio.removeEventListener("ended", handleEnded);
+  }, [playNext]);
+
+
+  useEffect(() => {
+    if (!audioRef.current || !currentSong) return;
+
+    if (prevSongRef.current !== currentSong.audioUrl) {
+      audioRef.current.src = currentSong.audioUrl;
+      audioRef.current.currentTime = 0;
+      prevSongRef.current = currentSong.audioUrl;
+
+   
+      if (isPlaying) {
+        audioRef.current.play();
+      }
+    }
+  }, [currentSong]); 
+  
+  return <audio ref={audioRef} />;
 }
 
 export default AudioPlayer
