c134f18b0ce99e46977538025cf9f931518cabae
Manos <manosnikitakis3@gmail.com>
2026-03-06 16:53:55 +0200

    fixed Express 5 error with *

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -64,9 +64,9 @@ app.use('/api/albums', albumRoutes);
 app.use('/api/stats', statRoutes);
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")))
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")))
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../Frontend/dist/index.html"))
   })
 }
 
