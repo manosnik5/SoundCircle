8f3a089a4dad3361714db509ddd731a13214019a
Manos <manosnikitakis3@gmail.com>
2026-03-06 15:49:54 +0200

    add production static file serving for frontend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -46,6 +46,13 @@ app.use('/api/songs', songRoutes);
 app.use('/api/albums', albumRoutes);
 app.use('/api/stats', statRoutes);
 
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
+  })
+}
+
 app.use((err, req, res, next) => {
   res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message });
 })
