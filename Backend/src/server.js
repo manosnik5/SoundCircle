06c6ea53aa721a986512191183c5112ba3211bee
Manos <manosnikitakis3@gmail.com>
2026-03-06 16:03:08 +0200

    add node-cron for deleting tmp files

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -13,6 +13,8 @@ import path from 'path';
 import cors from 'cors';
 import { createServer } from 'http';
 import { initializeSocket } from './lib/socket.js';
+import cron from "node-cron";
+import fs from "fs";
 
 dotenv.config();
 
@@ -37,6 +39,21 @@ app.use(fileupload({
         fileSize: 10 * 1024 * 1024
     }
 }));
+
+const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+	if (fs.existsSync(tempDir)) {
+		fs.readdir(tempDir, (err, files) => {
+			if (err) {
+				console.log("error", err);
+				return;
+			}
+			for (const file of files) {
+				fs.unlink(path.join(tempDir, file), (err) => {});
+			}
+		});
+	}
+});
 
 
 app.use('/api/users', userRoutes);
