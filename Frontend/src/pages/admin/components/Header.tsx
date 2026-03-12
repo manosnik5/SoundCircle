7fc5b4230d47d7d9ededbc3ad9d560cff46837b4
Manos <manosnikitakis3@gmail.com>
2026-03-12 13:04:12 +0200

    add soundCircle logo

--- Frontend/src/pages/admin/components/Header.tsx
+++ Frontend/src/pages/admin/components/Header.tsx
@@ -6,7 +6,8 @@ const Header = () => {
     <div className='flex items-center justify-between'>
       <div className='flex items-center gap-3 mb-8'>
         <Link to='/' className="rounded-lg">
-        <img src='/soundCircleLogo.png' className='size-15 text-black' /></Link>
+          <img src='/soundCircleLogo.png' className='size-15 text-black' />
+        </Link>
         <div>
           <h1 className="text-3xl font-bold">Music Manager</h1>
           <p className="text-zinc-400 mt-1">Manage your music catalog</p>
