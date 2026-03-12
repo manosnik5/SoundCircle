3919ce70c58a70cd07703a36430d892ef65a6f5b
Manos <manosnikitakis3@gmail.com>
2026-03-12 12:54:38 +0200

    change getAllUsers to getAllFriends and add searchUsers endpoint

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getAllUsers, getMessages } from "../controllers/user.controller.js";
+import { getAllFriends, getMessages, searchUsers } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.get("/", protectRoute, getAllUsers)
+router.get("/", protectRoute, getAllFriends)
+router.get('/search', protectRoute, searchUsers);
 router.get("/messages/:userId", protectRoute, getMessages)
 
 export default router;
