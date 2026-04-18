6f2ca434df0cce2ba404ce04df1d0dd52c81b583
Manos <manosnikitakis3@gmail.com>
2026-04-18 12:47:07 +0300

    Add togglePlay functionality when clicking anywhere in song container

--- Frontend/src/pages/home/components/FeaturedSection.tsx
+++ Frontend/src/pages/home/components/FeaturedSection.tsx
@@ -1,33 +1,53 @@
-import type { Song } from "@/types";
-import FeaturedGridSkeleton from "@/components/skeletons/FeaturedGridSkeleton";
-import PlayButton from "./PlayButton";
+import type { Song } from "@/types"
+import FeaturedGridSkeleton from "@/components/skeletons/FeaturedGridSkeleton"
+import PlayButton from "./PlayButton"
+import { usePlayer } from "@/contexts/MusicPlayerContext"
 
 interface FeaturedSectionProps {
-    songs: Song[];
-    isLoading: boolean;
+  songs: Song[]
+  isLoading: boolean
 }
 
-const FeaturedSection = ({songs, isLoading}: FeaturedSectionProps) => {
-    
-    if (isLoading) {
-        return <FeaturedGridSkeleton/>
-    }
+const FeaturedSection = ({ songs, isLoading }: FeaturedSectionProps) => {
+  const { currentSong, togglePlay, setCurrentSong } = usePlayer()
 
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
-            {songs?.map((featuredSong) => (
-                <div key={featuredSong._id} className="flex items-center bg-zinc-800/50 hover:bg-zinc-700/50 rounded-md overflow-hidden cursor-pointer group relative transition-colors">
-                    <img src={featuredSong.imageUrl} alt={featuredSong.title} className="w-16 sm:w-20 h-16 sm:h-20 object-cover shrink-0"/>
-                    <div className="flex-1 p-4">
-                        <p className="font-medium truncate">{featuredSong.title}</p>
-                        <p className="font-sm text-zinc-400 truncate">{featuredSong.artist}</p>     
-                    </div>
-                     <PlayButton song={featuredSong}/>
-                </div>
-              
-            ))}
+  if (isLoading) {
+    return <FeaturedGridSkeleton />
+  }
+
+  const handleCardClick = (song: Song) => {
+    const isCurrentSong = currentSong?._id === song._id
+
+    if (isCurrentSong) togglePlay()
+    else setCurrentSong(song)
+  }
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
+      {songs?.map((featuredSong) => (
+        <div
+          key={featuredSong._id}
+          onClick={() => handleCardClick(featuredSong)}
+          className="flex items-center bg-zinc-800/50 hover:bg-zinc-700/50 rounded-md overflow-hidden cursor-pointer group relative transition-colors"
+        >
+          <img
+            src={featuredSong.imageUrl}
+            alt={featuredSong.title}
+            className="w-16 sm:w-20 h-16 sm:h-20 object-cover shrink-0"
+          />
+
+          <div className="flex-1 p-4">
+            <p className="font-medium truncate">{featuredSong.title}</p>
+            <p className="text-sm text-zinc-400 truncate">
+              {featuredSong.artist}
+            </p>
+          </div>
+
+          <PlayButton song={featuredSong} />
         </div>
-    )
+      ))}
+    </div>
+  )
 }
 
 export default FeaturedSection
