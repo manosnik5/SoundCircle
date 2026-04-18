10327b3e1da4fa305df1f17f53f80ffab2cc617a
Manos <manosnikitakis3@gmail.com>
2026-04-18 12:46:05 +0300

    Change colors

--- Frontend/src/pages/admin/components/AlbumsTabContent.tsx
+++ Frontend/src/pages/admin/components/AlbumsTabContent.tsx
@@ -10,7 +10,7 @@ const AlbumsTabContent = () => {
         <div className="flex items-center justify-between">
           <div>
             <CardTitle className="flex items-center gap-2">
-              <Music className="size-5 text-emerald-500"/>
+              <Music className="size-5 text-[#694bcc]"/>
               Album Library
             </CardTitle>
             <CardDescription>Manage your music tracks</CardDescription>
